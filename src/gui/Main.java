b17dbf8d371319c16cfd6e97a82f875f84c17369
0VeRC0DeT <83798002+0VeRC0DeT@users.noreply.github.com>
2024-12-09 11:56:38 +0100

    hej

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -5,10 +5,9 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
         Application.launch(MainWindow.class);
-        //asfkls
     }
 
-
+/// hgdjbdjng
 
 
 }

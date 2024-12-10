55a23c456768bade9311360a87960d9339bbbf5f
moham <mohamedartam1@gmail.com>
2024-12-10 17:10:40 +0100

    tilføjer threePane.java

---
+++ src/gui/threePane.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class threePane extends Application{
+
+    //StoredataforPåfyldTønde
+    private final ListView<String>dataList=new ListView<>();
+
+    @Override
+    public void start(Stage stage){
+
+    }
+}
